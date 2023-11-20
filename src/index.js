32998f86277c7df5bbf6db680affab06998dba02
Luiz Henrique Coelho <127800714+Ligueja@users.noreply.github.com>
2023-11-20 00:26:09 -0300

    ajustes index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 import express from 'express';
-import bcrypt from "bcrypt";
+import bcrypt, { hash } from "bcrypt";
 import { randomUUID } from 'node:crypto';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {return response.json('OK');});
+app.get('/', (req, res) => {return res.json('OK');});
 
 const usuarios = [];
 const recados = [];
 
 
 // POST - PARA CRIAR USUÁRIO COM RECADO "FALSO"
-app.post ("/usuarios", async (req, resp) => {
-    const body= req.body;
+app.post ("/usuarios", async (req, res) => {
+    const body = req.body;
 
     if (body.nome === undefined) {
-        return resp.status(400).json("Nome não informado!");
+        return res.status(400).json("Nome não informado!");
     }
 
     if (body.email === undefined) {
-        return resp.status(400).json("E-mail não informado!");
+        return res.status(400).json("E-mail não informado!");
     }
 
     if (body.senha === undefined) {
-        return resp.status(400).json("Senha não informada!");
+        return res.status(400).json("Senha não informada!");
     }
 
     const verificarEmail = usuarios.find((usuario) => {
@@ -33,37 +33,71 @@ app.post ("/usuarios", async (req, resp) => {
     }); 
 
     if (verificarEmail !== undefined) {
-        return resp.status(400).json("E-mail já cadastrado!")
+        return res.status(400).json("E-mail já cadastrado!")
     }
 
-    const criptografarSenha = await bcrypt.hash(body.email, 6);
+    // const criptografarSenha = await bcrypt.hash(body.email, 6);
+
+    bcrypt.hash(senha, 6, (err, hash) => {
+        if (err) {
+           return res.status(500).json("Erro ao criar usuário!")
+        }
+        else {
+            const usuario = {
+                id: randomUUID(),
+                nome: body.nome,
+                email: body.email,
+                senha: hash,
+                recados: []
+            };
+        
+            usuarios.push(usuario);
+
+            console.log(usuario);
+        }
+    })
+
     
-    const usuario = {
-        id: randomUUID(),
-        nome: body.nome,
-        email: body.email,
-        senha: criptografarSenha,
-        recados: []
-    };
 
-    usuarios.push(usuario);
-
-    console.log(usuario);
-
-    return resp.status(201).json("Usuário criado com sucesso!");
+    return res.status(201).json("Usuário criado com sucesso!");
 
 })
 
 //  GET - PARA VISUALIZAR USUÁRIOS JÁ CADASTRADOS
-app.get("/usuarios", (req, resp) => {
+app.get("/usuarios", (req, res) => {
     
     if (usuarios.length < 1) {
-        return resp.status(404).json("Nnenhum usuário cadastrado!");
+        return res.status(404).json("Nnenhum usuário cadastrado!");
     }
 
-    return resp.json(usuarios);
+    return res.json(usuarios);
 
 });
+
+// POST - LOGIN DE USUÁRIO JÁ CADASTRADO 
+
+app.post("/usuarios/login", (req, res) => {
+    const login = req.body;
+
+    const usuario = usuarios.find((usuario) => usuario.email === login.email);
+
+    if (!usuario) {
+        return res.status(402).json("Usauário não cadastrado")
+    };
+
+    bcrypt.compare(login.senha, usuario.senha, function (err, result){
+        if (result) {
+            return res.status(200).json("Usuário logado");
+        }
+        else {
+            return res.status(402).json("Dados inválidos")
+        }
+    })
+});
+
+
+
+
 
 
 
